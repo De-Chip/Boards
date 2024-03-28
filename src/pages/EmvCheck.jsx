6bdaec7eb1772e15f73364a5eeaca803d79fd9a9
De-Chip <milkyice4567@naver.com>
2024-03-28 10:35:16 +0900

    env 활용

---
+++ src/pages/EmvCheck.jsx
@@ -0,0 +1,26 @@
+function EnvCheck(){
+    return(
+        <div>
+            <h1>환경변수 확인</h1>
+            
+            <div>
+                <h2>기본 환경 변수</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div>
+                <h2>사용자 정의 환경 변수</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default EnvCheck;
